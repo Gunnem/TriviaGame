1cfe5474a4b9cee569056d3622e1d22820bbd858
Matthew Kim <matthewkim@Matthews-iMac.local>
2018-10-17 01:50:58 -0700

    first pass

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+// Make global variables
+var expanse = {
+    setScreen: "",
+    rightCounter: 0,
+    wrongCounter: 0,
+    noAnsrCounter: 0,
+    clickSound: new Audio("assets/sounds/button-click.mp3"),
+    html: "",
+    questionArry: ["What was the Rocinante's original name?", "Who was Miller hired to find?", "What is the primary method of propulsion in the Expanse?", " What causes drinks to pour sideways on Tycho Station?", "What is the Ring object?"],
+    answerArry: ["Tachi"],
+    imgArry: [],
+    clock: "",
+    qCounter: 0,
+    timeCounter: 15,
+};
+
+// Functions
+
+function startButton(){
+    expanse.clock = setInterval(fifteenSec, 1000);
+    function fifteenSec(){
+        if(expanse.timeCounter === 0{
+
+        })
+    }
+}
+
