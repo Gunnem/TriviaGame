67165bfb088bd95a7f0eff0d49455dea7439ab17
Gunnem <sp3dng9@gmail.com>
2018-10-18 21:28:20 -0700

    rev

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@ var expanse = {
     qArry: ["What was the Rocinante's original name?", "Who was Miller hired to find?", "What is the primary method of propulsion in the Expanse?", " What causes drinks to pour sideways on Tycho Station?", "What is the Ring object?"],
     answerArry: [["Galileo", "Tachi", "Seung Un", "Razorback"],["Julie Mao", "Amos Burton", "Fred Johnson", "Drummer"], ["Artificial Singularity", "Warp Drive", "Epstein Drive", "Impulse Power"], ["Gravity Plating", "Statis Field", "Coanda Effect", "Coriolis Effect"], ["An alien monument", "A weapon", "A black hole", "A wormhole"]],
     rightAnswers: ["B. Tachi", "A. Julie Mao", "C. Epstein Drive", "D. Coriolis Effect", "D. A wormhole"],
-    imgArry: ["<img class='center-block img-right' src='/assets/images/roci.png'>", "<img class='center-block img-right' src='/assets/images/miller.png'>", "<img class='center-block img-right' src='/assets/images/epstein.png'>", "<img class='center-block img-right' src='/assets/images/tycho.png'>", "<img class='center-block img-right' src='/assets/images/ring.png'>",],
+    imgArry: ["<img class='center-block img-right' src='assets/images/roci.png'>", "<img class='center-block img-right' src='assets/images/miller.png'>", "<img class='center-block img-right' src='assets/images/epstein.png'>", "<img class='center-block img-right' src='assets/images/tycho.png'>", "<img class='center-block img-right' src='assets/images/ring.png'>",],
     clock: "",
     qCounter: 0,
     timeCounter: 15,
@@ -17,7 +17,7 @@ var expanse = {
 
 // Functions
 
-function startButton(){
+function startScreen(){
     expanse.setScreen = "<p class='text-center main-button'><a class='btn btn-primary btn-lg start-button text-center' href='#'>Begin</a></p>";
     $(".main-area").html(expanse.setScreen);
 };
@@ -25,15 +25,16 @@ function startButton(){
 function timer(){
     expanse.clock = setInterval(fifteenSecs, 1000);
     function fifteenSecs(){
-        if(expanse.timeCounter > 0) {expanse.timeCounter--;
-        }
-        else(expanse.timeCounter === 0){
-            timeOutLoss();
-            clearInterval(expanse.clock);
-        }
-        $(".timer").html(expanse.timeCounter);
+      if(expanse.timeCounter === 0){
+        timeOutLoss();
+        clearInterval(expanse.clock);
+      }
+      if(expanse.timeCounter > 0) {
+        expanse.timeCounter --;
+      }
+      $(".timer").html(expanse.timeCounter);
     }
-};
+  };
 
 function wait(){
     if(expanse.qCounter < 4){
@@ -49,7 +50,7 @@ function wait(){
 
 function win(){
     expanse.rightCounter ++;
-    expanse.gameHtml = "<p class='text-center'> Time Remaining: <span class='timer'>" + expanse.timeCounter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + expanse.rightAnswers[expanse.qCounter] + "</p>" + expanse.imgArray[expanse.qCounter];
+    expanse.gameHtml = "<p class='text-center'> Time Remaining: <span class='timer'>" + expanse.timeCounter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + expanse.rightAnswers[expanse.qCounter] + "</p>" + expanse.imgArry[expanse.qCounter];
     $(".main-area").html(expanse.gameHtml);
     setTimeout(wait, 4000);
 };
@@ -70,7 +71,7 @@ function loss(){
   
   function finalScreen(){
     expanse.gameHtml = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + expanse.timeCounter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + expanse.rightCounter + "</p>" + "<p>Wrong Answers: " + expanse.wrongCounter + "</p>" + "<p>Unanswered: " + expanse.noAnsrCounter + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
-    $(".main-area").html(expanse.gameHTML);
+    $(".main-area").html(expanse.gameHtml);
   };
 
   function resetGame(){
@@ -84,13 +85,13 @@ function loss(){
   };
   
   function generateHTML(){
-    expanse.gameHtml = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>20</span></p><p class='text-center'>" + expanse.qArry[expanse.qCounter] + "</p><button class='first-answer answer'>A. " + expanse.answerArry[expanse.qCounter][0] + "</button><br><button class='answer'>B. "+expanse.answerArry[expanse.qCounter][1]+"</button><br><button class='answer'>C. "+expanse.answerArry[expanse.qCounter][2]+"</button><br><button class='answer'>D. "+expanse.answerArry[expanse.qCounter][3]+"</button>";
-    $(".main-area").html(expanse.gameHTML);
+    expanse.gameHtml = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>15</span></p><p class='text-center'>" + expanse.qArry[expanse.qCounter] + "</p><button class='first-answer answer'>A. " + expanse.answerArry[expanse.qCounter][0] + "</button><br><button class='answer'>B. "+expanse.answerArry[expanse.qCounter][1]+"</button><br><button class='answer'>C. "+expanse.answerArry[expanse.qCounter][2]+"</button><br><button class='answer'>D. "+expanse.answerArry[expanse.qCounter][3]+"</button>";
+    $(".main-area").html(expanse.gameHtml);
   }
 
   //MAIN PROCESS
 
-  startButton();
+  startScreen();
 
 //start-button click
 $("body").on("click", ".start-button", function(event){
@@ -110,7 +111,7 @@ $("body").on("click", ".answer", function(event){
 		clearInterval(expanse.clock);
 		win();
 	}
-  //If incorrect ansewr
+  //If incorrect answer
 	else {
 
 		clearInterval(expanse.clock);
